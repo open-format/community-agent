ad7ca9b27e7d1bc540e0599edd53204f72a8bff8
andy <andy@openformat.tech>
2025-02-12 15:38:22 +0000

    feat: add GitHub webhook handling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+const app = new Hono();
 
-const app = new Hono()
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/webhooks/github", async (c) => {
+  const payload = await c.req.json();
+  console.log(payload);
+  if (payload?.commits?.length) {
+    return c.json({
+      message: `${payload.pusher.name} pushed to ${payload.repository.name}`,
+    });
+  }
+  return c.json({
+    message: "No commits found",
+  });
+});
 
-export default app
+export default app;
